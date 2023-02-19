614b8e86d0923620fa46d05d8ff9ffc531f043c4
IvanOliveiraaa <ivosn9@gmail.com>
2023-02-19 11:54:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
+    app.use(cors());
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    app.use(cors());
+
     next();
 });
 
