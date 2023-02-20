be27faa55b19d5ca1eedbaf5d18dfac0d1f7b950
IvanOliveiraaa <ivosn9@gmail.com>
2023-02-20 15:45:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    app.use(cors());
+
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
